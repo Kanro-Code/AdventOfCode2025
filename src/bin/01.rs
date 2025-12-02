ed9264dcd920007d9a3369a33c9fc9d186d3c0f5
Thijs ter Velde <tervelde.thijs@gmail.com>
2025-12-03 00:24:17 +0100

    Day 1 - Part 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,10 +1,58 @@
 advent_of_code::solution!(1);
 
-pub fn part_one(input: &str) -> Option<u64> {
-    None
+#[derive(Debug)]
+pub struct Instruction {
+    pub direction: Direction,
+    pub value: isize,
 }
 
-pub fn part_two(input: &str) -> Option<u64> {
+impl Instruction {
+    pub fn parse(s: &str) -> Self {
+        let (dir, val) = s.split_at(1);
+        let direction = match dir {
+            "L" => Direction::Left,
+            "R" => Direction::Right,
+            _ => panic!("Invalid direction"),
+        };
+        let value = val.parse::<isize>().expect("Invalid value");
+        Instruction { direction, value }
+    }
+
+    pub fn apply(&self, position: isize) -> isize {
+        match self.direction {
+            Direction::Left => position - self.value,
+            Direction::Right => position + self.value,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum Direction {
+    Left,
+    Right,
+}
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let instructions = input
+        .lines()
+        .map(Instruction::parse)
+        .collect::<Vec<Instruction>>();
+
+    let mut current_position = 50;
+    let mut count = 0;
+
+    for instruction in instructions {
+        current_position = instruction.apply(current_position).rem_euclid(100);
+
+        if current_position == 0 {
+            count += 1;
+        }
+    }
+
+    Some(count)
+}
+
+pub fn part_two(_input: &str) -> Option<u64> {
     None
 }
 
@@ -15,7 +63,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(3));
     }
 
     #[test]
